a4be407ff631bfd0d877400458f77a6335600835
ckwogus37 <ckwogus37@gmail.com>
2021-04-01 21:33:14 +0900

    ver 0.4

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
             <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <a class="nav-link active" aria-current="page" href="#">House</a>
                 </li>
                 <li class="nav-item">
                 <a class="nav-link" href="#">Features</a>
